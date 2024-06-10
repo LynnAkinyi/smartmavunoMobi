0fcc686b7dc41eff81d9e37061ec22681a05c0f4
LynnAkinyi <78434875+LynnAkinyi@users.noreply.github.com>
2024-06-10 15:33:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/fapp/createArticle.kt
@@ -0,0 +1,11 @@
+package com.example.fapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class createArticle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.`create_article.xml`)
+    }
+}
---
+++ app/src/main/java/com/example/fapp/settingsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class settingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.`settings.xml`)
+    }
+}
